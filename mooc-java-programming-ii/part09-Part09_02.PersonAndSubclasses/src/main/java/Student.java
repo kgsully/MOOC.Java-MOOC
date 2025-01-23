c3714d63ec3e1b36915b1c7eacad0b056b3544f5
Ken Sully <ken.sully@gmail.com>
2025-01-23 15:04:50 -0700

    WIP Completed some of the exercises from part 2-9-1, continuing with the remainder

---
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,26 @@
+
+
+public class Student extends Person{
+
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    public void study() {
+        this.credits += 1;
+    }
+
+    public int credits() {
+        return credits;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\n  " + "Study credits " + this.credits;
+    }
+
+
+}
